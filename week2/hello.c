30b8fdef71d9516e36e69aad2c8e2bd5a6eb3c32
Atul Kundaria <fiddle_dubnium_0c@icloud.com>
2022-08-27 11:03:31 +0530

    Hello C

--- week2/hello.c
+++ week2/hello.c
@@ -2,4 +2,10 @@
 
 int main (void){
   printf("Hello World!\n");
+  printf("Info is the name of the things!\n");
+  char name[10];
+  int age;
+  printf("Enter your first name and age: \n");
+  scanf("%s %d", name, &age); 
+  printf("You entered: %s %d",name,age);
 }
